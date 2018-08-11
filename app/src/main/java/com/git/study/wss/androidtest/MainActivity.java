721f7641cd680a3b8a7810ec658503b336b83a1f
DESKTOP-BDS4JNS\wss <wss520Android@163.com>
2018-08-11 15:55:46 +0800

    第二次更改develop代码

--- app/src/main/java/com/git/study/wss/androidtest/MainActivity.java
+++ app/src/main/java/com/git/study/wss/androidtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.git.study.wss.androidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv_one = findViewById(R.id.tv_one);
+        tv_one.setText("the develop is chang two ");
     }
 }
